c222ae3a11ceaff4928ef9635a4646c6e77b7ce6
eze110011 <ezequielmacchi@MacBook-Pro-de-Ezequiel-2.local>
2023-12-12 11:31:52 +0100

    Mover archivos a la misma carpeta que contiene index

--- prueba-libros/index.php
+++ prueba-libros/index.php
@@ -9,6 +9,23 @@
 </head>
 <body>
     <h1>Listado de Libros de Open Library por Género</h1>
-    <?php include 'functions.php';?>
+
+    <!-- Agrega un elemento donde se mostrará la respuesta de la función -->
+    <div id="result"></div>
+
+    <!-- Agrega un script para hacer la solicitud a la función -->
+    <script>
+        // Hacer una solicitud a la función de Netlify
+        fetch('https://tusitio.netlify.app/.netlify/functions/php-handler')
+            .then(response => response.json())
+            .then(data => {
+                // Mostrar la respuesta de la función en el elemento 'result'
+                document.getElementById('result').innerHTML = data.body;
+            })
+            .catch(error => {
+                console.error('Error al obtener la respuesta de la función:', error);
+            });
+    </script>
+
 </body>
 </html>
--- prueba-libros/php-handler.js
+++ prueba-libros/php-handler.js
@@ -1,13 +1,13 @@
 // netlify/functions/php-handler.js
-const { createServer } = require('http');
-const { spawnSync } = require('child_process');
-
 exports.handler = async function (event, context) {
-  const phpProcess = spawnSync('php', ['-r', 'echo "Hello from PHP!";']);
-  const phpOutput = phpProcess.stdout.toString();
-
   return {
     statusCode: 200,
-    body: `Hello from JavaScript and PHP! PHP says: ${phpOutput}`,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*', // Puedes restringir esto según tus necesidades
+    },
+    body: JSON.stringify({ message: 'Hello from Netlify Function!' }),
   };
 };
+
+
