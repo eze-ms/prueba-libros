e653fc1fbb3f054393fcdcc7b2893edcbc5ae462
eze110011 <ezequielmacchi@MacBook-Pro-de-Ezequiel-2.local>
2023-12-12 11:55:48 +0100

    Mover archivos a la misma carpeta que contiene index

--- prueba-libros/index.php
+++ prueba-libros/index.php
@@ -9,23 +9,6 @@
 </head>
 <body>
     <h1>Listado de Libros de Open Library por Género</h1>
-
-    <!-- Agrega un elemento donde se mostrará la respuesta de la función -->
-    <div id="result"></div>
-
-    <!-- Agrega un script para hacer la solicitud a la función -->
-    <script>
-        // Hacer una solicitud a la función de Netlify
-        fetch('https://tusitio.netlify.app/.netlify/functions/php-handler')
-            .then(response => response.json())
-            .then(data => {
-                // Mostrar la respuesta de la función en el elemento 'result'
-                document.getElementById('result').innerHTML = data.body;
-            })
-            .catch(error => {
-                console.error('Error al obtener la respuesta de la función:', error);
-            });
-    </script>
-
+    <?php include 'functions.php';?>
 </body>
 </html>
